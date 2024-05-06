bab2abc87e2b185b6af4e37f54aead6bfa5ea459
M4lcolmT <twhan06@gmail.com>
2024-05-06 13:53:57 +0800

    created LinkedList class

---
+++ LinkedList.h
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+// Self-created linked list class
+template <typename T>
+class LinkedList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& value) : data(value), next(nullptr) {}
+    };
+
+    Node* head;
+    Node* tail;
+    int size;
+
+public:
+    LinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    // Add more member functions as needed (e.g., append, remove, etc.)
+
+    // Function to add an element to the end of the list
+    void append(const T& value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    // Function to get the size of the list
+    int getSize() const {
+        return size;
+    }
+
+    // Function to access an element by index
+    T& operator[](int index) {
+        if (index < 0 || index >= size) {
+            throw std::out_of_range("Index out of range.");
+        }
+        Node* current = head;
+        for (int i = 0; i < index; i++) {
+            current = current->next;
+        }
+        return current->data;
+    }
+};
